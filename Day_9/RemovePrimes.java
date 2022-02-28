3dbfa73f33f4696982cb684e8c9c9f22b1794d5b
Shivani13121007 <shivani.gupta553955@gmail.com>
2022-03-01 00:20:28 +0530

    day9

---
+++ Day_9/RemovePrimes.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class RemovePrimes {
+
+	public static boolean isPrime(int val)
+	{
+		for(int div = 2; div*div <= val ;div++)
+		{
+			if(val % div == 0)
+			{
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+	public static void solution(ArrayList<Integer> al){
+		// write your code here
+		int n = al.size();
+
+		for(int i = n-1;i>=0;i--)
+		{
+			int val = al.get(i);
+			if(isPrime(val))
+			{
+				al.remove(i);
+			}
+		}
+		
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i = 0 ; i < n; i++){
+			al.add(scn.nextInt());
+		}
+		solution(al);
+		System.out.println(al);
+	}
+
+}
